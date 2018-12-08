015c1141b63c140ab37040194d9062c4a010ba81
Sachin <asachin96@yahoo.in>
2018-12-08 18:36:11 -0500

    final

--- example.py
+++ example.py
@@ -1,39 +1,31 @@
 import numpy as np
-import matcompat
+from math import log
+
 import random
 
-# if available import pylab (from matlibplot)
 from unseen import unseen
 from makeFinger import makeFinger
 from entropy_estC import entropy_estC
 
-# % Generate a sample of size 10,000 from the uniform distribution of support 100,000
 n = 100000
 k = 30000
-# samp = randi(n, k, 1.)
 samp = [random.uniform(0,n) for i in range(k)]
-# % Compute corresponding 'fingerprint'
 f = makeFinger(samp)
-# % Estimate histogram of distribution from which sample was drawn
 [h, x] = unseen(f)
-# %output entropy of the true distribution, Unif[n]
 trueEntropy = np.log(n)
-# %output entropy of the empirical distribution of the sample
+
 arr = []
 
-for y in range(0,len(f)):
+for y in range(1,len(f)+1):
     arr.append((y+1)/float(k))
 
-arr = np.dot(arr, np.log(arr))
-arr = arr.T
-empiricalEntropy = np.dot(-f.T, arr)
+for i in range(len(arr)):
+    arr[i] = arr[i]*log(arr[i])
 
-
-
-# %output entropy of the recovered histogram, [h,x]
+arr = np.array(arr).T
+empiricalEntropy = np.dot(-f.T, arr)[0]
 
 estimatedEntropy = np.matmul(-h,np.multiply(x, np.log(x)).T)
-# %output entropy using entropy_estC.m (should be almost the same as above):
 estimatedEntropy2 = entropy_estC(f)
 
-print(trueEntropy, empiricalEntropy, estimatedEntropy, estimatedEntropy2)
+print("TrueEntrop :" + str(trueEntropy), "EmpericalEntrop :" + str(empiricalEntropy), "EstimatedEntrop1 :" + str(estimatedEntropy), "EmpericalEntrop2 :" + str(estimatedEntropy2))
--- makeFinger.py
+++ makeFinger.py
@@ -1,32 +1,13 @@
-# Generated with SMOP  0.41
-from smop.libsmop import *
-# makeFinger.m
-from matplotlib.pyplot import hist
+import numpy as np
 
 
+def makeFinger(v=None, *args, **kwargs):
+    h1 = np.histogram(v, np.array(np.arange(np.min(v), np.max(v) + 2)))  # Random returns only 3 values
 
+    f = np.histogram(h1[0], np.array(np.arange(0, np.max(h1[0]) + 2)))
 
-@function
-def makeFinger(v=None,*args,**kwargs):
+    f = f[0][1:]
 
-    varargin = makeFinger.varargin
-    nargin = makeFinger.nargin
+    f = np.asanyarray(f).reshape(-1, 1)
 
-    # Input:  vector of integers, v
-# Output: vector of fingerprints, f where f(i) = |{j: |{k:v(k)=j}|=i }|
-#         i.e. f(i) is the number of elements that occur exactly i times
-
-#         in the vector v
-
-    h1=np.histogram(v,np.array(np.arange(np.min(v),np.max(v)+2))) #Random returns only 3 values
-
-# makeFinger.m:8
-    f=np.histogram(h1[0],np.array(np.arange(0 , np.max(h1[0])+2 )))
-# makeFinger.m:9
-
-    f=f[0][1:]#f(np.arange(2,end()))
-# makeFinger.m:10
-
-    f=ravel(f)
-# makeFinger.m:10
     return f
